3a9271dc45b050e4dd95053e5f4757f1bd192e67
taltara <taltarablus@gmail.com>
2020-10-17 14:47:43 +0300

    Updates

---
+++ precache-manifest.001e8f3652ba3eda9a10e5a9200df689.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184e854d533d07a7f49fc40465047ac7",
+    "url": "/BBallAgent/index.html"
+  },
+  {
+    "revision": "f5516272c330c8864c74",
+    "url": "/BBallAgent/static/css/main.7c06c583.chunk.css"
+  },
+  {
+    "revision": "787625068f3dc7953055",
+    "url": "/BBallAgent/static/js/2.e601b87f.chunk.js"
+  },
+  {
+    "revision": "bd02ac708d4c25359502212cad2481a3",
+    "url": "/BBallAgent/static/js/2.e601b87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5516272c330c8864c74",
+    "url": "/BBallAgent/static/js/main.e75dae7e.chunk.js"
+  },
+  {
+    "revision": "3b66e4e32be022c60ad0",
+    "url": "/BBallAgent/static/js/runtime-main.d5de0749.js"
+  },
+  {
+    "revision": "212f1e4f5857fabfd11c3475dc81ea3d",
+    "url": "/BBallAgent/static/media/animosa.212f1e4f.ttf"
+  },
+  {
+    "revision": "845c1121a361c41d60700eacbde61c74",
+    "url": "/BBallAgent/static/media/animosa.845c1121.woff2"
+  },
+  {
+    "revision": "a02bf812e9fcc3634ab45a0df20d39da",
+    "url": "/BBallAgent/static/media/animosa.a02bf812.woff"
+  },
+  {
+    "revision": "7bccb559dfd8eeadec30ac352f169eb5",
+    "url": "/BBallAgent/static/media/animosaExtraBold.7bccb559.woff2"
+  },
+  {
+    "revision": "8f7df5ca500f642444e7eb00f95546a3",
+    "url": "/BBallAgent/static/media/animosaExtraBold.8f7df5ca.woff"
+  },
+  {
+    "revision": "ea4d21bfae041a5ebe4ad6c3b0c043fc",
+    "url": "/BBallAgent/static/media/animosaExtraBold.ea4d21bf.ttf"
+  },
+  {
+    "revision": "16a7ffe73504db6dfd21a7380ad20774",
+    "url": "/BBallAgent/static/media/animosaExtraLight.16a7ffe7.woff"
+  },
+  {
+    "revision": "624439e879992dc9d18d1c089bb6e253",
+    "url": "/BBallAgent/static/media/animosaExtraLight.624439e8.woff2"
+  },
+  {
+    "revision": "bc117e67df06ea55997cce6517c284e2",
+    "url": "/BBallAgent/static/media/animosaExtraLight.bc117e67.ttf"
+  },
+  {
+    "revision": "cb750aeff95ed6e13a93d8afd5350489",
+    "url": "/BBallAgent/static/media/arrow.cb750aef.svg"
+  },
+  {
+    "revision": "b369a94eaa260be82c091a5eaed0774d",
+    "url": "/BBallAgent/static/media/back.b369a94e.svg"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/BBallAgent/static/media/heebo.3f5fef64.ttf"
+  },
+  {
+    "revision": "fd5e5f32c1f438ecf5dd71d6ae01b049",
+    "url": "/BBallAgent/static/media/heeboBold.fd5e5f32.ttf"
+  },
+  {
+    "revision": "35396eed7df7196dad5808bc7da3e2a3",
+    "url": "/BBallAgent/static/media/heeboExtraBold.35396eed.ttf"
+  },
+  {
+    "revision": "653e4d141e1ae41fffdcfc154e859848",
+    "url": "/BBallAgent/static/media/heeboExtraLight.653e4d14.ttf"
+  },
+  {
+    "revision": "29bad8e0b0e6cf92f1f7d22f7fcfba79",
+    "url": "/BBallAgent/static/media/heeboLight.29bad8e0.ttf"
+  },
+  {
+    "revision": "85cddc584ff9fa710e75e852a5cd14e6",
+    "url": "/BBallAgent/static/media/player.85cddc58.svg"
+  },
+  {
+    "revision": "6d53f9d908c6c617c2336934056225f0",
+    "url": "/BBallAgent/static/media/sort.6d53f9d9.svg"
+  }
+]);
